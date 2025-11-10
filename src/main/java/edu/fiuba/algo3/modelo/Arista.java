7aa7f138c8b7e92d8e8eafb9cd9780b1ff3c2db4
alejandrodelcs <adelcarpio@fi.uba.ar>
2025-11-10 09:46:37 -0300

    test de jugador realizado

--- src/main/java/edu/fiuba/algo3/modelo/Arista.java
+++ src/main/java/edu/fiuba/algo3/modelo/Arista.java
@@ -1,6 +1,6 @@
 package edu.fiuba.algo3.modelo;
 
 public class Arista {
-    public Arista(int x, int y, int z) {
+    public Arista() {
     }
 }
--- src/main/test/edu/fiuba/algo3/entrega_1/JugadorTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/JugadorTest.java
@@ -4,15 +4,12 @@ import edu.fiuba.algo3.modelo.Construccion.Carretera;
 import edu.fiuba.algo3.modelo.Jugador;
 import edu.fiuba.algo3.modelo.Construccion.Poblado;
 import edu.fiuba.algo3.modelo.Mapa;
-import edu.fiuba.algo3.modelo.Recurso.Lana;
-import edu.fiuba.algo3.modelo.Recurso.Madera;
 import edu.fiuba.algo3.modelo.Recurso.Recurso;
 import edu.fiuba.algo3.modelo.Arista;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-
 
 
 public class JugadorTest {
@@ -22,13 +19,13 @@ public class JugadorTest {
     ● Verificar que el lanzamiento de dados genere un número válido (2-12).*/
 
     @Test
-    public void test01ElJugadorDebeRecibir1LanaLuegoDeHaberColocadoSuSegundoPoblado(){
+    public void test01ElJugadorIniciaUnaPartidaDeberiaRecibirSusRecursosLuegoDeColocarElSegundoPoblado(){
         Mapa mapa = new Mapa();
         Jugador jugador = new Jugador("alejandro", mapa);
-        Poblado poblado1 = new Poblado( new Arista(1,-1,0));
-        Poblado poblado2 = new Poblado(new Arista(-1,1,0));
-        Carretera carretera1 = new Carretera(new Arista(0,-1,-1));
-        Carretera carretera2 = new Carretera(new Arista(-1,0,1));
+        Poblado poblado1 = new Poblado( new Arista());
+        Poblado poblado2 = new Poblado(new Arista());
+        Carretera carretera1 = new Carretera(new Arista());
+        Carretera carretera2 = new Carretera(new Arista());
 
         jugador.construir(poblado1);
         jugador.construir(carretera1);
@@ -36,12 +33,11 @@ public class JugadorTest {
         jugador.construir(poblado2);
 
 
-        ArrayList<Recurso> recursosEsperados = new ArrayList<>();
-        recursosEsperados.add(new Lana());
-        recursosEsperados.add(new Madera());
-
         ArrayList<Recurso> recursosObtenidos = jugador.construir(carretera2);
 
-        Assertions.assertEquals(recursosEsperados, recursosObtenidos);
+        recursosObtenidos.forEach(recurso ->
+                Assertions.assertEquals(1, recurso.obtenerRecurso())
+        );
+
     }
 }
