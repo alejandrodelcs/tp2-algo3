57c0e669bced77a6d179b6d9c5a8805208e232f9
Natanael <nbrizuela@fi.uba.ar>
2025-11-12 17:22:35 -0300

    Tests iniciales de las clases Inventario y Jugador

---
+++ src/main/test/edu/fiuba/algo3/jugador/InventarioTest.java
@@ -0,0 +1,51 @@
+package edu.fiuba.algo3.jugador;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+import java.util.Map;
+
+import edu.fiuba.algo3.modelo.Juego.Inventario;
+import edu.fiuba.algo3.modelo.Material.Costo;
+import edu.fiuba.algo3.modelo.Material.Madera;
+import edu.fiuba.algo3.modelo.Material.Recurso;
+import edu.fiuba.algo3.modelo.Material.TipoRecurso;
+
+/**
+ * InventarioTest
+ */
+public class InventarioTest {
+
+    @Test
+    public void test01CrearUnInventarioYAgregoUnRecurso() {
+
+        Inventario inventario1 = new Inventario();
+        TipoRecurso recursoMock = mock(TipoRecurso.class);
+
+        int cantEsperada = 1;
+        inventario1.agregarRecurso(recursoMock, 1);
+
+        assertEquals(cantEsperada, inventario1.contar(recursoMock));
+
+    }
+
+    @Test
+    public void test02UnInventarioPuedeConsumirUnRecurso() {
+        Inventario inventario1 = new Inventario();
+        Costo costoMock = mock(Costo.class);
+        Map<TipoRecurso, Integer> requisistos = Map.of(TipoRecurso.MADERA, 1);
+
+        when(costoMock.getRequisitos()).thenReturn(requisistos);
+
+        inventario1.agregarRecurso(TipoRecurso.MADERA, 3);
+
+        inventario1.consumirRecursos(costoMock);
+
+        int cantEsperada = 2;
+
+        assertEquals(cantEsperada, inventario1.contar(TipoRecurso.MADERA));
+    }
+
+}
---
+++ src/main/test/edu/fiuba/algo3/jugador/JugadorTest.java
@@ -0,0 +1,78 @@
+package edu.fiuba.algo3.jugador;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import edu.fiuba.algo3.modelo.Juego.Inventario;
+import edu.fiuba.algo3.modelo.Juego.Jugador;
+import edu.fiuba.algo3.modelo.Material.Costo;
+import edu.fiuba.algo3.modelo.Material.Recurso;
+import edu.fiuba.algo3.modelo.Material.TipoRecurso;
+
+/**
+ * JugadorTest
+ */
+public class JugadorTest {
+
+    @Test
+    public void test01CreoUnJugadorConUnNombre() {
+        Jugador jugador1 = new Jugador("Esteban");
+
+        String nombre = jugador1.getNombre();
+        String nombreEsperado = "Esteban";
+
+        assertEquals(nombreEsperado, nombre);
+
+    }
+
+    @Test
+    public void test02CrearJugadorYagregarUnInventarioConUnRecursoYPidoSuInventario() {
+
+        Jugador jugador1 = new Jugador("Esteban");
+        TipoRecurso recursoMock = mock(TipoRecurso.class);
+
+        jugador1.recibirRecurso(recursoMock, 2);
+
+        Inventario inventario1 = jugador1.getInventario();
+
+        assertEquals(inventario1.contar(recursoMock), 2);
+    }
+
+    @Test
+    public void test03JugadorNoPuedePagarUnConstruccion() {
+        Jugador jugador1 = new Jugador("Esteban");
+        jugador1.recibirRecurso(TipoRecurso.MADERA, 2);
+
+        Costo costoMOck = mock(Costo.class);
+        Map<TipoRecurso, Integer> requisito = Map.of(
+                TipoRecurso.MADERA, 3);
+
+        when(costoMOck.getRequisitos()).thenReturn(requisito);
+
+        assertFalse(jugador1.puedePagar(costoMOck));
+
+    }
+
+    @Test
+    public void test03JugadorPuedePagarUnConstruccion() {
+        Jugador jugador1 = new Jugador("Esteban");
+        jugador1.recibirRecurso(TipoRecurso.MADERA, 2);
+
+        Costo costoMOck = mock(Costo.class);
+        Map<TipoRecurso, Integer> requisito = Map.of(
+                TipoRecurso.MADERA, 2);
+
+        when(costoMOck.getRequisitos()).thenReturn(requisito);
+
+        assertTrue(jugador1.puedePagar(costoMOck));
+
+    }
+
+}
