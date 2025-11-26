d6d5d67f5737368cedaf885fcddf20092b0bc2a4
Jeronimo Olmedo <jeronimoolmedo14@gmail.com>
2025-11-26 20:47:57 -0300

    clase LanzamientoDados

---
+++ src/main/java/edu/fiuba/algo3/modelo/LanzamientoDados.java
@@ -0,0 +1,21 @@
+package edu.fiuba.algo3.modelo;
+
+public class LanzamientoDados {
+    private Dado dado1;
+    private Dado dado2;
+
+    public LanzamientoDados() {
+        this.dado1 = new Dado();
+        this.dado2 = new Dado();
+    }
+
+    // Constructor para testing
+    public LanzamientoDados(Dado d1, Dado d2) {
+        this.dado1 = d1;
+        this.dado2 = d2;
+    }
+
+    public int lanzar() {
+        return dado1.tirarDado() + dado2.tirarDado();
+    }
+}
