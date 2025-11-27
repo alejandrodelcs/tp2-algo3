df7dc6672350239f927e2366b03e0a1d48341320
alejandrodelcs <adelcarpio@fi.uba.ar>
2025-11-27 01:17:51 -0300

    test 03 lista recursos

--- src/main/java/edu/fiuba/algo3/modelo/Jugador.java
+++ src/main/java/edu/fiuba/algo3/modelo/Jugador.java
@@ -1,23 +1,27 @@
 package edu.fiuba.algo3.modelo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import edu.fiuba.algo3.modelo.Construcciones.*;
 import edu.fiuba.algo3.modelo.ElementosTablero.*;
-import edu.fiuba.algo3.modelo.Recurso.Recurso;
+import edu.fiuba.algo3.modelo.Recurso.*;
 
 /**
  * Jugador
  */
 public class Jugador {
 
-    private ArrayList<Recurso> cartasRecurso;
+    private List<Recurso> cartasRecurso;
     private ArrayList<Construccion> construcciones;
     private String nombre;
 
     public Jugador(String nombre) {
         this.nombre = nombre;
-        this.cartasRecurso = new ArrayList<>();
+        this.cartasRecurso = Arrays.asList(new Madera(), new Ladrillo(),
+                new Lana(), new Grano(), new Lana(),
+                new Ladrillo(), new Grano());
         this.construcciones = new ArrayList<>();
     }
 
@@ -43,5 +47,13 @@ public class Jugador {
             this.cartasRecurso.addAll(recursos);
 
         }
+    }
+
+    public List<Recurso> validarCartas(int valorDado) {
+        if (valorDado == 7 && cartasRecurso.size() >= 7){
+            return cartasRecurso.subList(0,Math.round((float) cartasRecurso.size() /2));
+        }
+
+        return null;
     }
 }
--- src/main/test/edu/fiuba/algo3/entrega_1/JugadorTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/JugadorTest.java
@@ -1,13 +1,18 @@
 package edu.fiuba.algo3.entrega_1;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
+import edu.fiuba.algo3.modelo.Recurso.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import edu.fiuba.algo3.modelo.*;
 import edu.fiuba.algo3.modelo.Construcciones.*;
 import edu.fiuba.algo3.modelo.ElementosTablero.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 /**
  * JugadorTest
@@ -48,11 +53,38 @@ public class JugadorTest {
 
         jugador.construir(vertice, new Poblado());
 
-        for (int i = 0; i > 10; i++) {
+        for (int i = 0; i < 10; i++) {
             jugador.generarSegunDado(dado);
         }
         assertEquals(10, jugador.cantidadCartas());
 
     }
+
+
+    /*
+        Verificar que si un jugador tiene más de 7 cartas, descarte correctamente la mitad,
+        redondeando hacia abajo, al lanzar un 7.
+        [1,2,3,4,5,6,7,8]
+     */
+
+    @Test
+    public void test03(){
+
+        //Arrange
+        Jugador jugador = new Jugador("Ale");
+        List<Recurso> p = Arrays.asList(new Madera(), new Ladrillo(),
+                                            new Lana(), new Grano(), new Lana(),
+                                           new Ladrillo(), new Grano());
+
+        //Act
+        List<Recurso> a = jugador.validarCartas(7);
+
+        //Assert
+        Assertions.assertNotEquals(p,a);
+
+
+    }
+
+
 
 }
