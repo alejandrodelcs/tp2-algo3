a023d66b8edd49166cac07548160d16b14df0b5e
Jeronimo Olmedo <jeronimoolmedo14@gmail.com>
2025-12-16 19:29:32 -0300

    mejorado Regla2a1Test

--- src/main/test/edu/fiuba/algo3/entrega_2/ReglaComercio2a1Test.java
+++ src/main/test/edu/fiuba/algo3/entrega_2/ReglaComercio2a1Test.java
@@ -6,14 +6,25 @@ import edu.fiuba.algo3.modelo.Excepciones.ComercioInvalido2a1;
 import edu.fiuba.algo3.modelo.Recurso.Grano;
 import edu.fiuba.algo3.modelo.Recurso.Ladrillo;
 import edu.fiuba.algo3.modelo.Recurso.Lana;
+import edu.fiuba.algo3.modelo.Recurso.Madera;
+
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ReglaComercio2a1Test {
+    @Test
+    public void test01SiLaCantidadYElTipoSonCorrectosNoDeberiaLanzarExcepcion(){
+        ReglaComercio regla = new ReglaComercio2a1(Ladrillo.class);
 
+        assertDoesNotThrow(() -> regla.validar(
+            List.of(Ladrillo.class, Ladrillo.class),
+            List.of(Lana.class)
+        ));
+    }
 
     @Test
     public void test02SiEntregaNoEsDelTipoCorrectoDeberiaLanzarUnaExcepcion() {
@@ -24,6 +35,19 @@ public class ReglaComercio2a1Test {
                 () -> regla.validar(
                         List.of(Ladrillo.class, Grano.class),
                         List.of(Lana.class)
+                )
+        );
+    }
+
+    @Test
+    public void test03SiElTipoEsCorrectoPeroLaCantidadNoLanzaExcepcion(){
+        ReglaComercio regla = new ReglaComercio2a1(Ladrillo.class);
+        
+        assertThrows(
+                ComercioInvalido2a1.class, 
+                () -> regla.validar(
+                    List.of(Ladrillo.class), 
+                    List.of(Madera.class)
                 )
         );
     }
