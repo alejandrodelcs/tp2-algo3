018115405e468c0b8c9e6364e4d82c6b6d701c3c
Jeronimo Olmedo <jeronimoolmedo14@gmail.com>
2025-12-19 18:42:10 -0300

    test cartaPV

--- src/main/test/edu/fiuba/algo3/entrega_2/CartaPVTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_2/CartaPVTest.java
@@ -1,6 +1,10 @@
 package edu.fiuba.algo3.entrega_2;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import edu.fiuba.algo3.modelo.Carta.CartaCaballero;
 import edu.fiuba.algo3.modelo.Carta.CartaPuntoVictoria;
 import edu.fiuba.algo3.modelo.Jugador.Inventario;
 import edu.fiuba.algo3.modelo.Jugador.Jugador;
@@ -27,5 +31,30 @@ public class CartaPVTest {
         carta.jugar(jugador, null);
 
         assertEquals(1, jugador.getPuntosVictoria());
+    }
+
+    @Test
+    public void test02CartaPuntoVictoriaNoEsDeUnSoloUso() {
+        assertFalse(carta.esDeUnSoloUso());
+    }
+
+    @Test
+    public void test04ManejoDeDisponibilidadHeredado() {
+        assertFalse(carta.estaDisponible());
+
+        carta.habilitar();
+        assertTrue(carta.estaDisponible());
+
+        carta.deshabilitar();
+        assertFalse(carta.estaDisponible());
+    }
+
+    @Test
+    public void test05EsDelMismoTipoQueFuncionaCorrectamente() {
+        CartaPuntoVictoria otraCartaPV = new CartaPuntoVictoria();
+        CartaCaballero cartaDiferente = new CartaCaballero();
+
+        assertTrue(carta.esDelMismoTipoQue(otraCartaPV));
+        assertFalse(carta.esDelMismoTipoQue(cartaDiferente));
     }
 }
